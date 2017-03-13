8f750e757db3415089b343b7ac6cb8a47bc8a5ba
Achilles Rasquinha <achillesrasquinha@gmail.com>
2017-03-13 23:30:03 +0530

    added base class - entity, stock and implemented plotting for share, initiated tests

--- bulbea/__init__.py
+++ bulbea/__init__.py
@@ -1,6 +1,8 @@
+# imports - compatibility packages
 from __future__ import absolute_import
 
-from bulbea.entity import Share
+# module imports
+from bulbea.entity import Share, Stock
 from bulbea.config import AppConfig
 from bulbea.app    import App
 from bulbea.learn  import sentiment
