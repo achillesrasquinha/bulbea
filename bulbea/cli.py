2f8fa330e0eea00e18bf77cf473e8cb996953005
Achilles Rasquinha <achillesrasquinha@gmail.com>
2017-03-13 23:31:22 +0530

    changes to GUI and CLI

--- bulbea/cli.py
+++ bulbea/cli.py
@@ -14,17 +14,14 @@ parser.add_argument('SOURCE',
                     help    = 'source code for economic data')
 parser.add_argument('SYMBOL',
                     help    = 'ticker symbol of a company')
-parser.add_argument('-a', '--alias',
-                    help    = 'alias name for a company (e.g. APPL - Apple)')
+parser.add_argument('-i', '--gui',
+                    action  = 'store_true',
+                    help    = 'launch the graphical user interface')
 parser.add_argument('-v', '--version',
                     action  = 'version',
                     version = 'v{version}'.format(version = '.'.join(map(str, AppConfig.VERSION))),
                     help    = 'show the version information and exit')
 
 def main():
-    args   = parser.parse_args()
-    source = args.SOURCE
-    symbol = args.SYMBOL
-    alias  = args.alias
-
-    share  = bb.Share(source, symbol, alias = alias)
+    args = parser.parse_args()
+    
