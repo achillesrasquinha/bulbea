f9b67630d5377c354830bbc408539a8ec5345bc0
Achilles Rasquinha <achillesrasquinha@gmail.com>
2017-03-15 00:05:55 +0530

    parameter checks for split

--- bulbea/learn/evaluation/split.py
+++ bulbea/learn/evaluation/split.py
@@ -5,15 +5,22 @@ from __future__ import absolute_import
 import numpy as np
 
 # module imports
-from bulbea._utils import _check_type, _check_int, _check_real, _check_iterable
+from bulbea._util import (
+    _check_type,
+    _check_int,
+    _check_real,
+    _check_iterable,
+    _validate_in_range
+)
+from bulbea.entity.share import _get_cummulative_return
 import bulbea as bb
 
 def _normalize(data):
-    normalized = np.array([ ((sample / sample[0]) - 1)for sample in data ])
+    normalized = np.array([ ((sample / sample[0]) - 1) for sample in data ])
     return normalized
 
 def split(share,
-          attr      = 'Close',
+          attrs     = 'Close',
           window    = 0.01,
           train     = 0.60,
           shift     = 1,
@@ -28,10 +35,10 @@ def split(share,
     _check_real(window, raise_err = True)
     _check_real(train, raise_err = True)
 
-    _check_in_range(window, 0, 1, raise_err = True)
-    _check_in_range(train, 0, 1, raise_err = True)
+    _validate_in_range(window, 0, 1, raise_err = True)
+    _validate_in_range(train, 0, 1, raise_err = True)
 
-    data   = share.data[attribute]
+    data   = share.data[attrs]
     length = len(share)
 
     window = int(np.rint(length * window))
@@ -40,7 +47,7 @@ def split(share,
     splits = np.array([data[i if i is 0 else i + offset: i + window] for i in range(length - window)])
 
     if normalize:
-        splits = _normalize(splits)
+        splits = np.array([_get_cummulative_return(split) for split in splits])
 
     size   = len(splits)
     split  = int(np.rint(train * size))
