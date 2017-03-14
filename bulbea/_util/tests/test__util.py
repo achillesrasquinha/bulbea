4944fead8e14cda7dfd2f070f49a7bce607c6ee5
Achilles Rasquinha <achillesrasquinha@gmail.com>
2017-03-14 08:53:54 +0530

    added tests cases for bulbea._util

---
+++ bulbea/_util/tests/test__util.py
@@ -0,0 +1,41 @@
+import pytest
+
+from bulbea._util import (
+    _raise_type_error,
+    _get_type_name,
+    _check_type,
+    _check_in_range,
+    _assign_if_none
+)
+
+def test__raise_type_error():
+    with pytest.raises(TypeError):
+        _raise_type_error(
+            expected_type_name = 'expected',
+            recieved_type_name = 'recieved'
+        )
+
+def test__get_type_name():
+    assert _get_type_name('foo') == 'str'
+    assert _get_type_name(12345) == 'int'
+    assert _get_type_name(1.234) == 'float'
+
+def test__check_type():
+    with pytest.raises(TypeError):
+        _check_type('foo', type_ = int, raise_err = True, expected_type_name = 'str')
+    with pytest.raises(TypeError):
+        _check_type(12345, type_ = str, raise_err = True, expected_type_name = 'int')
+
+    assert _check_type('bar', type_ = str) == True
+    assert _check_type('foo', type_ = int) == False
+
+def test__check_in_range():
+    with pytest.raises(ValueError):
+        _check_in_range(123, 0, 1, raise_err = True)
+
+    assert _check_in_range(0.5, 0, 1) == True
+    assert _check_in_range(123, 0, 1) == False
+
+def test__assign_if_none():
+    assert _assign_if_none(None, 1) == 1
+    assert _assign_if_none(1,'foo') == 1
