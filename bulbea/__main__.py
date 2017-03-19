80d555f83ddecb03440fce09b9499dadca24aee5
Achilles Rasquinha <achillesrasquinha@gmail.com>
2017-03-19 13:11:51 +0530

    added import comments

--- bulbea/__main__.py
+++ bulbea/__main__.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 
+# imports - compatibility imports
 from __future__ import absolute_import
 
+# imports - standard imports
 import sys
 
+# imports - module imports
 from bulbea.cli import main
 
 if __name__ == '__main__':
